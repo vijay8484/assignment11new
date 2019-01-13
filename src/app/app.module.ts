57de9423cd3c120d15a3c70e48cd4ae9402a9251
Your NameVijay <vijay8484patil@gmail.com>
2019-01-13 15:12:53 +0530

    first commit Assignment10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
+import { EmployeeReviewComponent } from './employee-review/employee-review.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { deLocale } from 'ngx-bootstrap/locale';
+defineLocale('de', deLocale); 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AlertModule,
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-review.ts
@@ -0,0 +1,17 @@
+export class EmployeeReview {
+
+
+    constructor(
+        public eFirstName : string,
+        public eLastName : string,
+        public DatePicker : string,
+        public eTitle : string,
+        public eDepartment : string,
+        public ePerform : boolean,
+        public detailData : string,
+        public sFirstName : string,
+        public sLastName : string
+    )
+    {
+    }
+}
