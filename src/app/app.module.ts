ef1e91f1953e6e13ede366ea7c8961a3af15d47d
Your NameVijay <vijay8484patil@gmail.com>
2019-01-19 03:01:56 +0530

    first commit Assignment11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { FormsModule } from '@angular/forms';
-import { AlertModule } from 'ngx-bootstrap';
-import { EmployeeReviewComponent } from './employee-review/employee-review.component';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { defineLocale } from 'ngx-bootstrap/chronos';
-import { deLocale } from 'ngx-bootstrap/locale';
-defineLocale('de', deLocale); 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeReviewComponent
+    ContactDetailsComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    AlertModule,
-    BsDatepickerModule.forRoot()
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
